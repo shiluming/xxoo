3182322d696ae59b27f4b93c3c42eede61b77e81
shiluming <luming_bfm@163.com>
2017-02-01 02:00:58 +0800

    clear

--- src/main/java/com/sise/dao/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.sise.dao;
-
-/**
- * Created by rola on 16-8-15.
- */
-public interface TestDao {
-
-    public void insertTest();
-}
--- src/main/java/com/sise/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.sise.service;
-
-/**
- * Created by rola on 16-8-15.
- */
-public interface TestService {
-
-    public void insetService();
-}
